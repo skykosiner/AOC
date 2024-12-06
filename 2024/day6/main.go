d7dcae1e0d781b1ec9cdefb1256c1649923a942d
Sky Kosiner <sky@skykosiner.com>
2024-12-06 10:00:52 +0000

    day6 part one, off by one classic

---
+++ 2024/day6/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/skykosiner/AOC/pkg/utils"
+)
+
+var directions = [][2]int{
+	{-1, 0}, // Up
+	{0, 1},  // Right
+	{1, 0},  // Down
+	{0, -1}, // Left
+}
+
+func walk(x, y, dir int, seen [][]bool, lines []string, visited *int) bool {
+	nextRow, nextCol := x+directions[dir][0], y+directions[dir][1]
+
+	if nextRow >= 0 && nextRow < len(lines) && nextCol >= 0 && nextCol < len(lines[nextRow]) && lines[nextRow][nextCol] == '#' {
+		newDir := (dir + 1) % 4
+		return walk(x, y, newDir, seen, lines, visited)
+	}
+
+	// Of the map
+	if nextRow < 0 || nextRow >= len(lines) || nextCol < 0 || nextCol >= len(lines[nextRow]) {
+		return true
+	}
+
+	// Seen before
+	if !seen[nextRow][nextCol] {
+		seen[nextRow][nextCol] = true
+		*visited++
+	}
+	return walk(nextRow, nextCol, dir, seen, lines, visited)
+}
+
+func main() {
+	// var sum1 int
+	var startX, startY int
+
+	lines := utils.ReadFile("./input")
+	seen := make([][]bool, len(lines))
+	for i := range seen {
+		seen[i] = make([]bool, len(lines[i]))
+	}
+
+	for x := range lines {
+		for y := range lines[x] {
+			if lines[x][y] == '^' {
+				startX, startY = x, y
+			}
+		}
+	}
+
+	visited := 1
+
+	if walk(startX, startY, 0, seen, lines, &visited) {
+		fmt.Println(visited)
+	} else {
+		fmt.Println("Can't solve??")
+	}
+}
