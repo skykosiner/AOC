1c63f6c80020af9fbd8c7507058a7bae0438f98c
Sky Kosiner <sky@skykosiner.com>
2024-12-09 10:33:13 +0000

    feat: Day 9 part two

--- 2024/day9/main.go
+++ 2024/day9/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"time"
 
@@ -19,30 +20,11 @@ func correct(blocks []string) bool {
 			return true
 		}
 	}
+
 	return true
 }
 
-func main() {
-	t := time.Now()
-	var blocks []string
-	sum := 0
-	line := utils.ReadFile("./input")[0]
-
-	currID := 0
-	for idx, char := range line {
-		if idx%2 == 0 {
-			for range int(char - '0') {
-				blocks = append(blocks, strconv.Itoa(currID))
-			}
-
-			currID++
-		} else {
-			for range int(char - '0') {
-				blocks = append(blocks, ".")
-			}
-		}
-	}
-
+func partOne(blocks []string) {
 	for {
 		for i := len(blocks) - 1; i >= 0; i-- {
 			if blocks[i] != "." {
@@ -52,7 +34,6 @@ func main() {
 					if blocks[j] == "." {
 						blocks[j] = block
 						blocks[i] = "."
-						fmt.Println(blocks)
 						break
 					}
 				}
@@ -65,17 +46,140 @@ func main() {
 			break
 		}
 	}
+}
 
-	for idx, block := range blocks {
+func findFreePoints(blocks []string) [][]int {
+	points := make([][]int, 0)
+
+	length := 0
+	start := -1
+
+	for i, block := range blocks {
+		if block == "." {
+			if start == -1 {
+				start = i
+			}
+
+			length++
+		} else {
+			if length > 0 {
+				points = append(points, []int{start, length})
+			}
+			start = -1
+			length = 0
+		}
+	}
+	// Add last point if it exists
+	if length > 0 {
+		points = append(points, []int{start, length})
+	}
+
+	return points
+}
+
+func reverse(slice []int) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+func partTwo(blocks []string) {
+	files := make(map[int]int)
+
+	for _, block := range blocks {
+		if block != "." {
+			num, _ := strconv.Atoi(block)
+			files[num] += 1
+		}
+	}
+
+	var ids []int
+
+	for key := range files {
+		ids = append(ids, key)
+	}
+
+	slices.Sort(ids)
+	reverse(ids)
+
+	for _, id := range ids {
+		points := findFreePoints(blocks)
+		for _, point := range points {
+			start, length := point[0], point[1]
+			// Check if the free space is large enough
+			if length >= files[id] {
+				// Move the file to the free space
+				for i := 0; i < files[id]; i++ {
+					blocks[start+i] = strconv.Itoa(id)
+				}
+
+				// Clear the original file blocks
+				c := 0
+				for i := len(blocks) - 1; i >= 0; i-- {
+					if blocks[i] == strconv.Itoa(id) {
+						blocks[i] = "."
+						c++
+						if c == files[id] {
+							break
+						}
+					}
+				}
+				break
+			}
+		}
+	}
+}
+
+func main() {
+	t := time.Now()
+
+	var blocksOne []string
+	var blocksTwo []string
+
+	sum1 := 0
+	sum2 := 0
+	line := utils.ReadFile("./input")[0]
+
+	currID := 0
+	for idx, char := range line {
+		if idx%2 == 0 {
+			for range int(char - '0') {
+				blocksOne = append(blocksOne, strconv.Itoa(currID))
+				blocksTwo = append(blocksTwo, strconv.Itoa(currID))
+			}
+
+			currID++
+		} else {
+			for range int(char - '0') {
+				blocksOne = append(blocksOne, ".")
+				blocksTwo = append(blocksTwo, ".")
+			}
+		}
+	}
+
+	partOne(blocksOne)
+	partTwo(blocksTwo)
+
+	for idx, block := range blocksOne {
 		num, err := strconv.Atoi(block)
 		if err != nil {
 			continue
 		}
 
-		sum += idx * num
+		sum1 += idx * num
 	}
 
-	fmt.Println(sum)
+	fmt.Println(blocksTwo)
+
+	for i, block := range blocksTwo {
+		if block != "." {
+			num, _ := strconv.Atoi(block)
+			sum2 += i * num
+		}
+	}
+
+	fmt.Println(sum1)
+	fmt.Println(sum2)
 	since := time.Since(t)
 	fmt.Println(since.Minutes(), since.Seconds())
 }
