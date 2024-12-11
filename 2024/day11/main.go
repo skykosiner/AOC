f497a5e8eb7450e973a37010891f0217bc80bcea
Sky Kosiner <sky@skykosiner.com>
2024-12-11 08:30:08 +0000

    clean up

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -8,7 +8,37 @@ import (
 	"github.com/skykosiner/AOC/pkg/utils"
 )
 
-func partOne() int {
+func sum(m map[int]int) int {
+	sum := 0
+	for _, count := range m {
+		sum += count
+	}
+
+	return sum
+}
+
+func loop(stones map[int]int, cache map[int]int) {
+	for stone, count := range stones {
+		if stone == 0 {
+			cache[1] += count
+			continue
+		} else {
+			str := strconv.Itoa(stone)
+			if len(str)%2 == 0 {
+				mid := len(str) / 2
+				first, _ := strconv.Atoi(str[:mid])
+				second, _ := strconv.Atoi(str[mid:])
+				cache[first] += count
+				cache[second] += count
+			} else {
+				multiplied := stone * 2024
+				cache[multiplied] += count
+			}
+		}
+	}
+}
+
+func main() {
 	line := utils.ReadFile("./input")[0]
 	stones := make(map[int]int)
 
@@ -19,78 +49,26 @@ func partOne() int {
 
 	for range 25 {
 		cache := make(map[int]int)
-		for stone, count := range stones {
-			if stone == 0 {
-				cache[1] += count
-				continue
-			} else {
-				str := strconv.Itoa(stone)
-				if len(str)%2 == 0 {
-					mid := len(str) / 2
-					first, _ := strconv.Atoi(str[:mid])
-					second, _ := strconv.Atoi(str[mid:])
-					cache[first] += count
-					cache[second] += count
-				} else {
-					multiplied := stone * 2024
-					cache[multiplied] += count
-				}
-			}
-		}
-
+		loop(stones, cache)
 		stones = cache
 	}
+	fmt.Println("Part One", utils.Sum(stones))
 
-	sum := 0
-	for _, count := range stones {
-		sum += count
-	}
-
-	return sum
-}
-
-func partTwo() int {
-	line := utils.ReadFile("./input")[0]
-	stones := make(map[int]int)
-
+	stones = make(map[int]int)
 	for _, num := range strings.Split(line, " ") {
 		n, _ := strconv.Atoi(num)
 		stones[n]++
 	}
-
 	for range 75 {
 		cache := make(map[int]int)
-		for stone, count := range stones {
-			if stone == 0 {
-				cache[1] += count
-				continue
-			} else {
-				str := strconv.Itoa(stone)
-				if len(str)%2 == 0 {
-					mid := len(str) / 2
-					first, _ := strconv.Atoi(str[:mid])
-					second, _ := strconv.Atoi(str[mid:])
-					cache[first] += count
-					cache[second] += count
-				} else {
-					multiplied := stone * 2024
-					cache[multiplied] += count
-				}
-			}
-		}
-
+		loop(stones, cache)
 		stones = cache
 	}
 
-	sum := 0
+	sumTwo := 0
 	for _, count := range stones {
-		sum += count
+		sumTwo += count
 	}
 
-	return sum
-}
-
-func main() {
-	fmt.Println("Part One:", partOne())
-	fmt.Println("Part Two:", partTwo())
+	fmt.Println("Part Two", sumTwo)
 }
--- 2024/day9/clean.go
+++ 2024/day9/clean.go
@@ -1,58 +1,59 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/skykosiner/AOC/pkg/utils"
-)
-
-func main() {
-	var disk []int
-	fid := 0
-
-	for idx, char := range utils.ReadFile("./input.test")[0] {
-		x := int(char - '0')
-		if idx%2 == 0 {
-			for range x {
-				disk = append(disk, fid)
-			}
-
-			fid++
-		} else {
-			for range x {
-				disk = append(disk, -1)
-			}
-		}
-	}
-
-	var blanks []int
-
-	for idx, x := range disk {
-		if x == -1 {
-			blanks = append(blanks, idx)
-		}
-	}
-
-	for _, blank := range blanks {
-		for len(disk) > 0 && disk[len(disk)-1] == -1 {
-			disk = disk[:len(disk)-1]
-		}
-
-		if len(disk) <= blank {
-			break
-		}
-
-		lastElement := disk[len(disk)-1]
-		disk = disk[:len(disk)-1]
-
-		disk[blank] = lastElement
-		fmt.Println(disk)
-	}
-
-	total := 0
-	for idx, num := range disk {
-		total += num * idx
-	}
-
-	fmt.Println(total)
-}
+//
+// import (
+// 	"fmt"
+//
+// 	"github.com/skykosiner/AOC/pkg/utils"
+// )
+//
+// func main() {
+// 	var disk []int
+// 	fid := 0
+//
+// 	for idx, char := range utils.ReadFile("./input.test")[0] {
+// 		x := int(char - '0')
+// 		if idx%2 == 0 {
+// 			for range x {
+// 				disk = append(disk, fid)
+// 			}
+//
+// 			fid++
+// 		} else {
+// 			for range x {
+// 				disk = append(disk, -1)
+// 			}
+// 		}
+// 	}
+//
+// 	var blanks []int
+//
+// 	for idx, x := range disk {
+// 		if x == -1 {
+// 			blanks = append(blanks, idx)
+// 		}
+// 	}
+//
+// 	for _, blank := range blanks {
+// 		for len(disk) > 0 && disk[len(disk)-1] == -1 {
+// 			disk = disk[:len(disk)-1]
+// 		}
+//
+// 		if len(disk) <= blank {
+// 			break
+// 		}
+//
+// 		lastElement := disk[len(disk)-1]
+// 		disk = disk[:len(disk)-1]
+//
+// 		disk[blank] = lastElement
+// 		fmt.Println(disk)
+// 	}
+//
+// 	total := 0
+// 	for idx, num := range disk {
+// 		total += num * idx
+// 	}
+//
+// 	fmt.Println(total)
+// }
